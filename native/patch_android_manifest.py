0a7623474a24d55dc5e6711d9ef7734dfe0af94e
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-09-21 16:46:14 +0100

    Declare launcher package visibility for update detection

--- native/patch_android_manifest.py
+++ native/patch_android_manifest.py
@@ -4,6 +4,9 @@ import sys
 
 p=Path(sys.argv[1])
 s=p.read_text()
+if "<queries>" not in s:
+    s=re.sub(r"(<manifest\\b[^>]*>)", r'\\1\\n    <queries><intent><action android:name="android.intent.action.MAIN" /><category android:name="android.intent.category.LAUNCHER" /></intent></queries>', s, count=1)
+
 if "REQUEST_INSTALL_PACKAGES" not in s:
     s=re.sub(r"(<manifest\\b[^>]*>)", r'\\1\\n    <uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES" />', s, count=1)
 if "InstallResultReceiver" not in s:
