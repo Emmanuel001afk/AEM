0e7e401bb4485a2afc872495137f9afa067c1ade
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-09-21 16:51:58 +0100

    Allow AEM to inspect installed apps for update matching

--- native/patch_android_manifest.py
+++ native/patch_android_manifest.py
@@ -9,6 +9,9 @@ if "<queries>" not in s:
 
 if "REQUEST_INSTALL_PACKAGES" not in s:
     s=re.sub(r"(<manifest\\b[^>]*>)", r'\\1\\n    <uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES" />', s, count=1)
+if "QUERY_ALL_PACKAGES" not in s:
+    s=re.sub(r"(<manifest\\b[^>]*>)", r"\\1\\n    <uses-permission android:name=\"android.permission.QUERY_ALL_PACKAGES\" />", s, count=1)
+
 if "InstallResultReceiver" not in s:
     s=s.replace("</application>", '    <receiver android:name=".InstallResultReceiver" android:exported="false" />\\n</application>', 1)
 p.write_text(s)
