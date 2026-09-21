cb6729f7c0ca3097754221b3c3b22f976b797681
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-09-21 17:00:31 +0100

    Correct manifest replacement groups

--- native/patch_android_manifest.py
+++ native/patch_android_manifest.py
@@ -5,10 +5,10 @@ import sys
 p=Path(sys.argv[1])
 s=p.read_text()
 if "<queries>" not in s:
-    s=re.sub(r"(<manifest\b[^>]*>)", r'\\1\\n    <queries><intent><action android:name="android.intent.action.MAIN" /><category android:name="android.intent.category.LAUNCHER" /></intent></queries>', s, count=1)
+    s=re.sub(r"(<manifest\b[^>]*>)", r'\1\n    <queries><intent><action android:name="android.intent.action.MAIN" /><category android:name="android.intent.category.LAUNCHER" /></intent></queries>', s, count=1)
 
 if "REQUEST_INSTALL_PACKAGES" not in s:
-    s=re.sub(r"(<manifest\\b[^>]*>)", r'\\1\\n    <uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES" />', s, count=1)
+    s=re.sub(r"(<manifest\\b[^>]*>)", r'\1\n    <uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES" />', s, count=1)
 if "QUERY_ALL_PACKAGES" not in s:
     s=re.sub(r"(<manifest\\b[^>]*>)", r"\1\n    <uses-permission android:name=\"android.permission.QUERY_ALL_PACKAGES\" />", s, count=1)
 
