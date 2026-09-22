0592facd77a11de78609b07d2e44c6ba3fc12b72
Emmanuel Akintunde <akintundeemmanuel14@gmail.com>
2026-09-22 20:15:54 +0100

    Use Capacitor bridge for the Android store client

--- native/MainActivity.java
+++ native/MainActivity.java
@@ -1,10 +1,11 @@
 package com.aem.store;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         registerPlugin(AemInstallerPlugin.class);
         super.onCreate(savedInstanceState);
     }
